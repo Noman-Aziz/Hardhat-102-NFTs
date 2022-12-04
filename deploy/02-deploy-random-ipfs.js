a1469920684c102f55921db82f0b32751e962855
Noman Aziz <l.nomanaziz@pm.me>
2022-12-04 20:54:30 +0500

    Added RandomIPFSNFT Contract, DeploymentScript, Tests

---
+++ deploy/02-deploy-random-ipfs.js
@@ -0,0 +1,115 @@
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata")
+
+const FUND_AMOUNT = "1000000000000000000000" // 10 LINK
+const imagesLocation = "./images/randomNft/"
+
+let tokenUris = [
+    "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo",
+    "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d",
+    "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm",
+]
+
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_type: "Cuteness",
+            value: 100,
+        },
+    ],
+}
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    // Get the IPFS hashes of our images
+    // 1. With our own IPFS node. https://docs.ipfs.io
+    // 2. Pinata https://www.pinata.cloud
+    // 3. NFT.storage https://nft.storage
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
+
+    let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock
+
+    if (developmentChains.includes(network.name)) {
+        // create VRFV2 Subscription
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait()
+        subscriptionId = txReceipt.events[0].args.subId
+        // Fund the subscription
+        // Our mock makes it so we don't actually have to worry about sending fund
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+
+    log("----------------------------------------------------")
+
+    arguments = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId]["gasLane"],
+        networkConfig[chainId]["callbackGasLimit"],
+        tokenUris,
+        networkConfig[chainId]["mintFee"],
+    ]
+
+    const randomIpfsNft = await deploy("RandomIpfsNft", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (developmentChains.includes(network.name)) {
+        await vrfCoordinatorV2Mock.addConsumer(subscriptionId, randomIpfsNft.address)
+    }
+
+    log("----------------------------------------------------")
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(randomIpfsNft.address, arguments)
+    }
+}
+
+async function handleTokenUris() {
+    tokenUris = []
+
+    // 1. Store the image in IPFS
+    // 2. Store the metadata in IPFS
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation)
+
+    for (imageUploadResponseIndex in imageUploadResponses) {
+        // 1. Create Metadata
+
+        let tokenUriMetadata = { ...metadataTemplate }
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+
+        // 2. Upload Metadata
+
+        console.log(`Uploading ${tokenUriMetadata.name}...`)
+        const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata)
+        tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`)
+    }
+
+    console.log("Token URIs uploaded! They are:")
+    console.log(tokenUris)
+    return tokenUris
+}
+
+module.exports.tags = ["all", "randomipfs", "main"]
